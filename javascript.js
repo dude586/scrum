7284d2f226cf2c6801b851ad9ef5fedc3a08d33b
dude586 <36538808+dude586@users.noreply.github.com>
2019-12-11 15:47:04 +0100

    Delete javascript.js

--- javascript.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let url = 'https://scrumserver.tenobe.org/scrum/api/bericht/read.php?profielId=2';
-fetch(url)
-    .then(function (response){return response.json();})
-    .then(function (data){console.log(data);});
