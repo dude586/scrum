0075febfe2f26d94475f7a93275f83c44896767f
Deusbladex <58769084+Deusbladex@users.noreply.github.com>
2019-12-12 14:58:25 +0100

    update

--- js/alternatief.js
+++ js/alternatief.js
@@ -1,10 +1,14 @@
 "use strict";
+let zoekresultaten = [];
 
-document.getElementById("knoptesten").onclick = function(){
+document.getElementById("toevoegen").onclick = function(){
     let url='https://scrumserver.tenobe.org/scrum/api/profiel/search.php?sexe=m';
                 //LET OP : rooturl = https://scrumserver.tenobe.org/scrum/api
                 fetch(url)
                     .then(function (resp)   { return resp.json(); })
-                    .then(function (data)   { console.log(data[0].voornaam);  })
+                    .then(function (data)   {  for(const p of data){
+                        zoekresultaten.push = p;
+                    } })
                     .catch(function (error) { console.log(error); });
 };
+console.log(zoekresultaten);
