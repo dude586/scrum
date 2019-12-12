28418b4024609b91acc9005e38489ac91d4b72c6
Mai <mai_tayar@yahoo.com>
2019-12-12 15:14:21 +0100

    inchrijven.html bijna klaar..upload button nog niet

--- js/inschrijvingFormulier.js
+++ js/inschrijvingFormulier.js
@@ -1,22 +1,54 @@
 "use strict"
 
-const user = [
-{
-	beroep: "Dj",
-	email: "gabriel@hotmail.com",
-	familienaam: "Vaneenoo",
-	foto: "jv.png",
-	geboortedatum: "1954-12-14",
-	gewicht: "88",
-	grootte: "184",
-	haarkleur: "blond",
-	id: "1",
-	lovecoins: "0",
-	nickname: "doetje",
-	oogkleur: "groen-grijs",
-	sexe: "m",
-	voornaam: "Jozef",
-	wachtwoord: "1020",
-}];
+/*valideren onchange**********************/
 
-console.log(user);
+const invoerelementen=document.querySelectorAll("input,select");
+for(const element of invoerelementen){
+    element.onchange=function(){
+        if(this.checkValidity()){
+            this.classList.remove("fout");
+        }else{this.classList.add("fout");}//this.style.borderColor="#f00";
+
+        
+    }
+}
+
+//upload button onclick
+document.getElementById("upload").onclick=function(){
+window.open("https://scrumserver.tenobe.org/scrum/frontend/fotoWebcam.html");
+}
+
+//verzenden button onclick
+document.getElementById("submit").onclick=function(){
+	
+	//valideren
+	const eersteInvalid=document.querySelector("input:invalid,select:invalid");
+    const herhaalWachtwoord=document.getElementById("h-wachtwoord");
+
+	
+	//controleer eerst als alle gegeven werd ingevoerd
+	if(eersteInvalid !==null ){ /*er is ivalid invoer*/
+	 
+		eersteInvalid.classList.add("fout"); 
+		
+	}else{
+		//controleer als de wachtwoord werd het zelfde herhaald
+	    
+	    if(document.getElementById("wachtwoord").value !==herhaalWachtwoord.value){ //niet het zelde
+	     	
+			herhaalWachtwoord.classList.add("fout");
+			
+		}else{
+		
+			herhaalWachtwoord.classList.remove("fout");
+
+			//hier stuur de gegevens
+	
+
+		}
+		  
+	}
+
+	
+	
+}
