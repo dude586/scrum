1225762e7c5041a04ba541106ed1d9ea060e4d6f
Deusbladex <58769084+Deusbladex@users.noreply.github.com>
2019-12-13 09:37:06 +0100

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,14 @@
 "use strict"
+alleDivid = ["login","profiel",zoek","uitvoer"];
+
+function toonDIV(divid);
+{   for (let teller = 0; teller < alleDivid.length; teller++) 
+     {const elemDividtmp = document.getElementById(alleDivid[teller]);
+      elemDividtmp.style.display="none";}
+    const elemDivid = document.getElementById(divid);
+    elemDivid.style.display="block";
+    
+
+     
+}
+
