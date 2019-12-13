33d8c9107e11d28fbde39ee8c9fe938326be22dc
Deusbladex <58769084+Deusbladex@users.noreply.github.com>
2019-12-13 10:36:09 +0100

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,27 @@
 "use strict"
-alleDivid = ["login","profiel",zoek","uitvoer"];
+let alleDivid = ["login","profiel","zoek","zoekresults"];
 
-function toonDIV(divid);
+function toonDIV(divid)
 {   for (let teller = 0; teller < alleDivid.length; teller++) 
      {const elemDividtmp = document.getElementById(alleDivid[teller]);
+      console.log(alleDivid[teller]);
       elemDividtmp.style.display="none";}
-    const elemDivid = document.getElementById(divid);
-    elemDivid.style.display="block";
+    if (divid !== "none" ) {const elemDivid = document.getElementById(divid);
+    elemDivid.style.display="block";}
     
 
      
 }
 
+
+const buttontest1 = document.getElementById("test1");
+const buttontest2 = document.getElementById("test2");
+
+buttontest1.onclick = function() {
+  toonDIV("none");
+}
+
+
+buttontest2.onclick = function() {
+  toonDIV("zoek");
+}
